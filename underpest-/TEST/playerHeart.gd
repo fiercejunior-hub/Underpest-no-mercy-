b04a03212e16f8533720209a733a0560835a042f
cemisDev <cemisdev@gmail.com>
2026-08-30 23:16:22 -0300

    enemy attack setup

---
+++ underpest-/TEST/playerHeart.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@onready var box : NinePatchRect = $"../heartBox"
+const marginUp = Vector2(24,24)
+const marginDown = -Vector2(28,28)
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_vector("izquierda","derecha","arriba","abajo")
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+	move_and_slide()
+	position = position.clamp(marginUp - box.size/2, marginDown + box.size/2)
